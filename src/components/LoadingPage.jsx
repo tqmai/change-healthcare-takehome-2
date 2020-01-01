d7ecbb0641f83bab798b0314cc81aa915ad8f731
Timothy Mai <timothy.mai13@gmail.com>
2020-01-01 00:55:12 -0500

    added notes for LoadingPage, updated title on index.html

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -10,3 +10,8 @@
  */
 
 // add to this later
+// use fetch/axios to get json data from games.json
+
+// https://create-react-app.dev/docs/fetching-data-with-ajax-requests
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+// https://github.com/axios/axios
