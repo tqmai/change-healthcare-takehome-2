85a01fb7f3937f07a6da84d3a495856c76c5f74d
Timothy Mai <timothy.mai13@gmail.com>
2020-01-03 09:13:03 -0800

    added file for SearchBar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,13 @@
+/**
+ * ************************************
+ *
+ * @module  SearchBar.jsx
+ * @author Timothy Mai
+ * @date 1/2/20
+ * @description displays game information
+ *
+ * ************************************
+ */
+
+import React from 'react';
+
