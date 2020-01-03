9b2d693876587b23c66a0a01f9b313105e01d0d5
Timothy Mai <timothy.mai13@gmail.com>
2020-01-02 22:49:54 -0800

    completed ExtendedGameDetails component

--- src/components/ExtendedGameDetails.jsx
+++ src/components/ExtendedGameDetails.jsx
@@ -10,15 +10,64 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class ExtendedGameDetails extends React.Component {
-  render() {
-    return (
+/*
+Component must show the following data
+- Game slug
+- List of game file names
+- List of category section names
+*/
+
+function ExtendedGameDetails(props) {
+  /* props contains the following:
+  - slug (string)
+  - gameFiles (array of strings)
+  - categorySections (array of strings)
+  */
+
+  const {
+    slug,
+    gameFiles,
+    categorySections,
+  } = props;
+
+  const gameFilesLi = gameFiles.map((file) => <li>{file}</li>);
+
+  const categorySectionsLi = categorySections.map((cat) => <li>{cat}</li>);
+
+  return (
+    <>
+      <p>
+        Slug:
+        {` ${slug}`}
+      </p>
+
       <div>
-        <p>more game details</p>
+        <p>
+          Game Files:
+        </p>
+        <ul>
+          {gameFilesLi.length ? gameFilesLi : <li>None</li>}
+        </ul>
       </div>
-    )
-  }
+
+      <div>
+        <p>
+          Category Sections:
+        </p>
+        <ul>
+          {categorySectionsLi.length ? gameFilesLi : <li>None</li>}
+        </ul>
+      </div>
+    </>
+  );
 }
+
+ExtendedGameDetails.propTypes = {
+  slug: PropTypes.string.isRequired,
+  gameFiles: PropTypes.arrayOf(PropTypes.string).isRequired,
+  categorySections: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default ExtendedGameDetails;
