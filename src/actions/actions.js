d93d1e02746895f5de99aa03e8460adf02c83e8b
Timothy Mai <timothy.mai13@gmail.com>
2020-01-03 21:16:56 -0800

    added action creators

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,15 +2,45 @@
  * ************************************
  *
  * @module  actions.js
- * @author
- * @date
- * @description Action Creators
+ * @author Timothy Mai
+ * @date 1/3/20
+ * @description action creators
  *
  * ************************************
  */
 
 // // import actionType constants
 // import * as types from '../constants/actionTypes'
+import * as types from '../constants/actionTypes';
+
+/*
+
+export const ADD_GAME_DATA = 'ADD_GAME_DATA';
+export const UPDATE_SEARCH_QUERY = 'UPDATE_SEARCH_QUERY';
+export const TOGGLE_ADD_ONS = 'TOGGLE_ADD_ONS';
+export const TOGGLE_VOICE = 'TOGGLE_VOICE';
+
+*/
+
+export const addGameData = (gameData) => ({
+  type: types.ADD_GAME_DATA,
+  payload: gameData,
+});
+
+export const updateSearchQuery = (query) => ({
+  type: types.UPDATE_SEARCH_QUERY,
+  payload: query,
+});
+
+export const toggleAddOns = () => ({
+  type: types.TOGGLE_ADD_ONS,
+  payload: null,
+});
+
+export const toggleVoice = () => ({
+  type: types.TOGGLE_VOICE,
+  payload: null,
+});
 
 // // add card needs to be passed a marketId from our dispatch function
 // // we will then pass along our marketId as a payload to the marketsReducer
@@ -36,4 +66,3 @@
 // })
 
 
-// ADD THIS LATER
