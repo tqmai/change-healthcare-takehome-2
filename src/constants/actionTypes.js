f6a2b7900e8141e1795b087cb4e8874ff05f75e3
Timothy Mai <timothy.mai13@gmail.com>
2020-01-04 11:45:10 -0800

    added additional action type constants for game data loading

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -9,7 +9,12 @@
  * ************************************
  */
 
+// for game data loading
 export const ADD_GAME_DATA = 'ADD_GAME_DATA';
+export const SET_TOTAL_NUMBER_OF_GAMES = 'SET_TOTAL_NUMBER_OF_GAMES';
+export const TOGGLE_LOADING_STATE = 'TOGGLE_LOADING_STATE';
+
+// for search bar functionality
 export const UPDATE_SEARCH_QUERY = 'UPDATE_SEARCH_QUERY';
 export const TOGGLE_ADD_ONS = 'TOGGLE_ADD_ONS';
 export const TOGGLE_VOICE = 'TOGGLE_VOICE';
