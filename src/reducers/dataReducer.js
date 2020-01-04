f81226412ca485618aea9e80f1b0a9984180d8bc
Timothy Mai <timothy.mai13@gmail.com>
2020-01-03 22:00:42 -0800

    added temporary state

--- src/reducers/dataReducer.js
+++ src/reducers/dataReducer.js
@@ -11,7 +11,47 @@
 
 import * as types from '../constants/actionTypes';
 
-function dataReducer(state = {}, action) {
+const gamesData = {
+  4: {
+    ID: 1,
+    name: 'World of Warcraft',
+    supportsAddons: true,
+    supportsVoice: false,
+    slug: 'wow',
+    gameFiles: [
+      {
+        Id: 1,
+        FileName: 'file1',
+      },
+      {
+        Id: 2,
+        FileName: 'file2',
+      },
+    ],
+    categorySections: [
+      {
+        ID: 1,
+        Name: 'cat1',
+      },
+    ],
+  },
+  3: {
+    ID: 10,
+    name: 'Coding Boi',
+    supportsAddons: false,
+    supportsVoice: true,
+    slug: 'cbi',
+    gameFiles: [
+      {
+        Id: 1,
+        FileName: 'file1',
+      },
+    ],
+    categorySections: [],
+  },
+};
+
+function dataReducer(state = gamesData, action) { // UPDATE THIS LATER TO START WITH AN EMPTY OBJECT
   switch (action.type) {
     case types.ADD_GAME_DATA:
       return {
