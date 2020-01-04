0a36a35ca8dfac34fd70909cfcacd71ca419fd3f
Timothy Mai <timothy.mai13@gmail.com>
2020-01-03 22:34:40 -0800

    add redux dev tool

--- src/store.js
+++ src/store.js
@@ -9,22 +9,12 @@
  * ************************************
  */
 
-// import { createStore } from 'redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-// import reducers from './reducers/index';
-
-import { createStore } from 'redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers/index';
 
-// // we are adding composeWithDevTools here to get easy access to the Redux dev tools
-// const store = createStore(
-//   reducers,
-//   composeWithDevTools()
-// );
-
-const store = createStore(reducers);
-
-// export default store;
+const store = createStore(reducers, composeWithDevTools(
+  // applyMiddleware(...middleware), // uncomment this line if we add middleware
+));
 
 export default store;
