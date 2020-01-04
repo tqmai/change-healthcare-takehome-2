a2ac47c2093471a4a66d0140861a0ff91e508458
Timothy Mai <timothy.mai13@gmail.com>
2020-01-03 22:37:46 -0800

    removed comments

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -9,18 +9,7 @@
  * ************************************
  */
 
-// // import actionType constants
-// import * as types from '../constants/actionTypes'
 import * as types from '../constants/actionTypes';
-
-/*
-
-export const ADD_GAME_DATA = 'ADD_GAME_DATA';
-export const UPDATE_SEARCH_QUERY = 'UPDATE_SEARCH_QUERY';
-export const TOGGLE_ADD_ONS = 'TOGGLE_ADD_ONS';
-export const TOGGLE_VOICE = 'TOGGLE_VOICE';
-
-*/
 
 export const addGameData = (gameData) => ({
   type: types.ADD_GAME_DATA,
@@ -41,28 +30,3 @@ export const toggleVoice = () => ({
   type: types.TOGGLE_VOICE,
   payload: null,
 });
-
-// // add card needs to be passed a marketId from our dispatch function
-// // we will then pass along our marketId as a payload to the marketsReducer
-// export const addCard = (marketId) => ({
-//   type: types.ADD_CARD,
-//   payload: marketId
-// });
-
-// export const deleteCard = (marketId) => ({
-//   type: types.DELETE_CARD,
-//   payload: marketId
-// });
-
-// export const addMarket = () => ({
-//   type: types.ADD_MARKET,
-//   payload: null
-// });
-
-
-// export const setNewLocation = (location) => ({
-//   type: types.SET_NEW_LOCATION,
-//   payload: location
-// })
-
-
