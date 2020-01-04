b8f9a892102123afe6175574bd6f74e85fd5ca8a
Timothy Mai <timothy.mai13@gmail.com>
2020-01-03 21:16:17 -0800

    added action type constants

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -2,24 +2,14 @@
  * ************************************
  *
  * @module  actionTypes.js
- * @author
- * @date
- * @description Action Type Constants
+ * @author Timothy Mai
+ * @date 1/3/20
+ * @description action type constants
  *
  * ************************************
  */
 
-/* 
-  Why use action types at all??
-  - in a small project it might not be necessary! but the overhead is small enough to almost always warrant using them
-  - consistent naming across project
-  - helpful to reference existing actions when working on features to prevent duplicate code
-  - when trying to dispatch an imported action, if you spell it incorrectly it will throw a reference error immediately, instead of sending an incorrect dispatch which will do nothing. Much easier for debugging!
-*/
-
-// export const ADD_CARD = "ADD_CARD";
-// export const DELETE_CARD = "DELETE_CARD";
-// export const ADD_MARKET = "ADD_MARKET";
-// export const SET_NEW_LOCATION = "SET_NEW_LOCATION";
-
-// ADD THIS LATER
+export const ADD_GAME_DATA = 'ADD_GAME_DATA';
+export const UPDATE_SEARCH_QUERY = 'UPDATE_SEARCH_QUERY';
+export const TOGGLE_ADD_ONS = 'TOGGLE_ADD_ONS';
+export const TOGGLE_VOICE = 'TOGGLE_VOICE';
